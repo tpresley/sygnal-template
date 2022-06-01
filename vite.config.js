57b59d88f38fe247428a965073b7887a3bea86ab
Troy Presley <troy.presley@gmail.com>
2022-06-01 00:43:09 -1000

    use new sygnal/jsx pragma

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
    esbuild: {
-     jsxFactory: `Snabbdom.createElement`,
-     jsxInject: `import Snabbdom from 'snabbdom-pragma'`,
+     jsxFactory: `jsx`,
+     jsxInject: `import { jsx } from 'sygnal/jsx'`,
    }
 });
